1675863480adef44db788e42fcb8332c639ec769
shenxiang <shenxiang@foursmile001.com>
2024-01-26 13:09:38 +0800

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by FS on 2024/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
